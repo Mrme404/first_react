57d37572608a029f9e791e66d897d4a90ab8504b
Mrme404 <kunkamiki@gmail.com>
2020-09-23 08:33:41 +0700

    2 files modified

--- index.js
+++ index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
+const cinta = "Joice";
+const panggil = <p>Hai sayangku {cinta} </p>;
+const tanya = <p>Kamu lagi apa sayangku ? </p>;
+let ulang = 0;
+
 ReactDOM.render(
-  <h1>HAI Joice ku  </h1>,
+  panggil,
   document.getElementById("test")
-)
+);
+ReactDOM.render(
+  tanya,
+  document.getElementById("tanya")
+);
+
+function tampil(){
+  ulang++;
+  const show = <p>{ulang}</p>;
+  ReactDOM.render(
+    show,
+    document.getElementById("he")
+  )
+}
+setInterval(tampil, 1000);
